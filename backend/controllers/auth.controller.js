e5565ab5b769c76ca93be54e629efc623164b982
Pranjalbharat <pranjalbharat07@gmail.com>
2024-06-23 19:08:17 +0530

    Backend Done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup= async(req,res)=>{
 try{
@@ -14,6 +16,9 @@ if(user){
     return res.status(400).json({ error: "Username already exists" });
 }
 
+const salt= await bcrypt.genSalt(10);
+const hashedPassword=await bcrypt.hash(password,salt);
+
 
 // https://avatar-placeholder.iran.liara.run/
 
@@ -23,12 +28,15 @@ const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${use
 const newUser = new User({
     fullName,
     username,
-    password,
+    password: hashedPassword,
     gender,
     profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
 });
 
-await newUser.save();
+if(newUser){
+    
+    generateTokenAndSetCookie(newUser._id,res);
+    await newUser.save();
 
 res.status(201).json({
 	_id: newUser._id,
@@ -36,6 +44,9 @@ res.status(201).json({
 	username: newUser.username,
 	profilePic: newUser.profilePic,
 			});
+}else {
+    res.status(400).json({error: "Inavlid user data"});
+}
 }
 catch(error){
     console.log("Error in signup controller", error.message);
@@ -43,10 +54,39 @@ catch(error){
 }
 };
 
-export const login = (req,res)=>{
-    console.log("loginUser");
+export const login = async (req,res)=>{
+    try {
+        const {username,password}=req.body;
+        const user=await User.findOne({username});
+        const isPassword=await bcrypt.compare(password,user?.password || "");
+
+        if(!user || !isPassword){
+            return res.status(400).json({error: "Invalid username or password"})
+        }
+        
+        generateTokenAndSetCookie(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            username:user.user,
+            profilePic:user.profilePic,
+        }
+
+        )
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
-export const logout =(req,res)=>{
-    console.log("logoutUser");
+export const logout =async (req,res)=>{
+   try {
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({messgae: "Looged out sucessfully"});
+    
+   } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+   }
 }
